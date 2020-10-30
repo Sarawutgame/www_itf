38ea6c0fa08dfa0f5509ccf078ec8fba361842b0
Sarawutgame <73683067+Sarawutgame@users.noreply.github.com>
2020-10-30 12:46:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <html>
     <head>
-        <title>Hello ITF</title>
+        <meta charset="UTF-8">
+        <title>Hello Cat</title>
     </head>
     <body>
-        <h1>My Cat Heading</h1>
-        <p>My first Cat</p>
-        <img src="Cat.jpg">
+        <h1>Cat and my com</h1>
+        <p>=====เเมว=====</p>
+        <img src="Cat2.jpg">
+        <img src="win.png">
+        <img src="plug.jpg">
         <br>
         <a href="http://www.it.kmitl.ac.th">This is link </a>
+        <a href="https://www.google.com/">IS Not Yahoo </a>
 
-        <h2>The unordered list</h2>
+        <h2>เรื่องราวที่เกิดขึ้น</h2>
         <ul>
-            <li>Google</li>
-            <li>Amazon</li>
-            <li>Yahoo</li>
+            <li>ขณะที่ผมกำลังUpdate windows อยู่</li>
+            <li>ก็มีเเมวเดินมาข้างหลังคอมเเล้วกะโจนใสปลักไฟ</li>
+            <li>เเล้วผมก็ตะโกนลั้ยเลยครับ</li>
+            <li>จริงๆก็ไม่มีไรหรอกเเค่เเมวโดดไปจับจิ่งจกเฉยๆ</li>
+            <li>เเค่ร้องตกใจว่าบ้านไฟไหม้เเค่นั้นเอง</li>
         </ul>
+        <img src="Cat3.jpg">
     </body>
 </html>
