4969f6b01b856aa2b2cf94a7e4f2a9d95444e808
Sarawutgame <73683067+Sarawutgame@users.noreply.github.com>
2020-10-30 12:24:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Jallo world!";
-?>
+<html>
+    <head>
+        <title>Hello ITF</title>
+    </head>
+    <body>
+        <h1>My Cat Heading</h1>
+        <p>My first Cat</p>
+        <img src="Cat.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is link </a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Amazon</li>
+            <li>Yahoo</li>
+        </ul>
+    </body>
+</html>
